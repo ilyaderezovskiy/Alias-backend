d71b500cf19a5f283229a9f392486074baae1543
ilyaderezovskiy <71149562+ilyaderezovskiy@users.noreply.github.com>
2023-04-07 02:52:58 +0300

    Add files via upload

---
+++ Alias/Sources/App/Migrations/CreateRooms.swift
@@ -0,0 +1,30 @@
+//
+//  CreateRooms.swift
+//  
+//
+//  Created by Ilya Derezovskiy on 06.04.2023.
+//
+
+import Fluent
+import Vapor
+
+struct CreateRooms: AsyncMigration {
+    func prepare(on database: Database) async throws {
+        try await database.schema("rooms")
+            .id()
+            .field("adminID", .uuid, .required)
+            .field("playersID", .array(of: .uuid), .required)
+            .field("difficulty", .int64, .required)
+            .field("isOpen", .bool, .required)
+            .field("code", .int64)
+            .field("points", .int64, .required)
+            .field("gameTime", .int64, .required)
+            .field("teamsCount", .int64, .required)
+            .create()
+    }
+
+    func revert(on database: Database) async throws {
+        try await database.schema("rooms").delete()
+    }
+}
+
---
+++ Alias/Sources/App/Models/User.swift
@@ -0,0 +1,47 @@
+//
+//  File.swift
+//  
+//
+//  Created by Ilya Derezovskiy on 28.03.2023.
+//
+
+import Fluent
+import Vapor
+
+final class User: Model {
+    static let schema = "users"
+    
+    @ID(key: "id")
+    var id: UUID?
+    
+    @Field(key: "username")
+    var username: String
+    
+    @Field(key: "password_hash")
+    var passwordHash: String
+    
+    final class Public: Content {
+        var id: UUID?
+        var username: String
+        
+        init(id: UUID? = nil, username: String) {
+            self.id = id
+            self.username = username
+        }
+    }
+}
+
+extension User {
+    func asPublic() -> User.Public {
+        User.Public(id: id, username: username)
+    }
+}
+
+extension User: ModelAuthenticatable {
+    static let usernameKey = \User.$username
+    static let passwordHashKey = \User.$passwordHash
+
+    func verify(password: String) throws -> Bool {
+        try Bcrypt.verify(password, created: self.passwordHash)
+    }
+}
